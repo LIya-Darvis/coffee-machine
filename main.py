6185f2e9b6bf20eaacc7e3864fb9e99ab178ceaa
Liya Darvis <57328715+LIya-Darvis@users.noreply.github.com>
2020-06-24 14:40:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 def buy():
     print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
     coffee = input()
-
-    global water
-    global milk
-    global beans
-    global disposable_cups
-    global money
     
     while True:
         if coffee == '1':
             if water >= 250 and beans >=16 and disposable_cups >= 1:
                 print("I have enough resources, making you a coffee!")
                 print("")
-                water = water - 250
-                beans = beans - 16
-                disposable_cups = disposable_cups - 1
-                money = money + 4
+                water -= 250
+                beans -= 16
+                disposable_cups -= 1
+                money += 4
             else:
                 if water < 250:
                     print("Sorry, not enough water!")
@@ -33,11 +27,11 @@ def buy():
             if water >= 350 and milk >= 75 and beans >= 20 and disposable_cups >= 1:
                 print("I have enough resources, making you a coffee!")
                 print("")
-                water = water - 350
-                milk = milk - 75
-                beans = beans - 20
-                disposable_cups = disposable_cups - 1
-                money = money + 7
+                water -= 350
+                milk -= 75
+                beans -= 20
+                disposable_cups -= 1
+                money += 7
             else:
                 if water < 350:
                     print("Sorry, not enough water!")
@@ -57,11 +51,11 @@ def buy():
             if water >= 200 and milk >= 100 and beans >= 12 and disposable_cups >= 1:
                 print("I have enough resources, making you a coffee!")
                 print("")
-                water = water - 200
-                milk = milk - 100
-                beans = beans - 12
-                disposable_cups = disposable_cups - 1
-                money = money + 6
+                water -= 200
+                milk -= 100
+                beans -= 12
+                disposable_cups -= 1
+                money += 6
             else:
                 if water < 200:
                     print("Sorry, not enough water!")
@@ -92,21 +86,15 @@ def fill():
     disposable_cups_extra = int(input())
     print("")
     
-    global water
-    global milk
-    global beans
-    global disposable_cups
-    
-    water = water + water_extra
-    milk = milk + milk_extra
-    beans = beans + beans_extra
-    disposable_cups = disposable_cups + disposable_cups_extra
+    water += water_extra
+    milk += milk_extra
+    beans += beans_extra
+    disposable_cups += disposable_cups_extra
 
 ##----------------------------------------##
 
 def take():
-    global money
-    print("I gave you $" + str(money))
+    print(f"I gave you $ {money}")
     print("")
     money = 0
 
@@ -114,18 +102,12 @@ def take():
 
 def remaining():
     
-    global water
-    global milk
-    global beans
-    global disposable_cups
-    global money
-    
     print("The coffee machine has:")
-    print(str(water) + " of water")
-    print(str(milk) + " of milk")
-    print(str(beans) + " of coffee beans")
-    print(str(disposable_cups) + " of disposable cups")
-    print(str(money) + " of money")
+    print(f"{water} of water")
+    print(f"{milk} of milk")
+    print(f"{beans} of coffee beans")
+    print(f"{disposable_cups} of disposable cups")
+    print(f"{money} of money")
     print("")
 
 ##----------------------------------------##
